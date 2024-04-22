cd39e814f3c63dfe8ec2ae7e9b56ac4d720bcbe9
Fran Salanova <fsalanova.16@gmail.com>
2024-04-22 20:01:50 -0300

    Adding initial unit tests

--- minesweeper.test.js
+++ minesweeper.test.js
@@ -1,7 +1,37 @@
-// import { createBoard } from "./minesweeper.js"
+import {
+  createBoard,
+  markedTilesCount,
+  TILE_STATUSES,
+  markTile,
+} from "./minesweeper.js"
 
-// describe("#createBoard", () => {
-//   test("returns a brand new board", () => {
-//     expect(createBoard(10, [(3, 6), (2, 9), (6, 4)])).toHaveReturned()
-//   })
-// })
+// Tablero base para ejecutar las pruebas unitarias
+const boardSize = 2
+const minePositions = [{ x: 0, y: 1 }]
+const board = createBoard(boardSize, minePositions)
+
+describe("#createBoard", () => {
+  test("returns a brand new board", () => {
+    const expectedBoard = [
+      [
+        { x: 0, y: 0, mine: false, status: TILE_STATUSES.HIDDEN },
+        { x: 0, y: 1, mine: true, status: TILE_STATUSES.HIDDEN },
+      ],
+      [
+        { x: 1, y: 0, mine: false, status: TILE_STATUSES.HIDDEN },
+        { x: 1, y: 1, mine: false, status: TILE_STATUSES.HIDDEN },
+      ],
+    ]
+    expect(board).toEqual(expectedBoard)
+  })
+})
+
+describe("#markedTilesCount", () => {
+  test("returns the count of marked tiles on the board", () => {
+    expect(markedTilesCount(markTile(board, { x: 0, y: 0 }))).toBe(1)
+  })
+
+  test("with no tiles marked", () => {
+    expect(markedTilesCount(board)).toBe(0)
+  })
+})
